ec6847e0b842dfdc6329cbf379d43350731b2738
momenbh <mdmomenbhuiya@gmail.com>
2024-03-19 10:48:28 +0600

    all steps in comment  added

--- test.php
+++ test.php
@@ -17,16 +17,16 @@
     //Handling get request to fetch data 
     if($_SERVER['REQUEST_METHOD'] === 'GET'){ //Checking the HTTP METHOD if it's GET
         $sql= "SELECT *  FROM  products ORDER BY id " ; // $sql = retrieving data form database using sql command 
-        $result = mysqli_query($link, $sql);
+        $result = mysqli_query($link, $sql); // runs query after checking db connection
 
-        if (mysqli_num_rows($result) > 0) {
-            $products = array();
-            while ($row = mysqli_fetch_assoc($result)) {
-                $products[] = $row;
+        if (mysqli_num_rows($result) > 0) { //_num_rows checks data row and returns if > 0
+            $products = array(); // initialize an empty array
+            while ($row = mysqli_fetch_assoc($result)) { //fetch_assoc gets the corrent row & move the pointer to next
+                $products[] = $row; //storing the result from row to a variable
             }
-            echo json_encode($products);
+            echo json_encode($products); //encode the array to json format and show output
         } else {
-            echo json_encode(array('message' => 'No products found.'));
+            echo json_encode(array('message' => 'No products found.')); 
         }
     }
 ?>
